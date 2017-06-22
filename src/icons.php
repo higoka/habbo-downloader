471f429b8b4c8739e3d41e9ec7fab411a5242ee2
Tafelglotzer <nico.rickenbach@gmail.com>
2017-06-22 23:42:07 +0200

    Added icons downloader

---
+++ src/icons.php
@@ -0,0 +1,14 @@
+<?php
+
+if (! is_dir('resources/icons')) {
+    mkdir('resources/icons', 0777, true);
+}
+
+for ($i = 1; $i <= 300; $i++) {
+    $url  = sprintf('https://images.habbo.com/c_images/catalogue/icon_%d.png', $i);
+    $file = sprintf('resources/icons/icon_%d.png', $i);
+
+    $files[$file] = $url;
+}
+
+download($files);
