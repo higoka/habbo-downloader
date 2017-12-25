320bb9bbe49e729b8053a57751fc59ce27de81cf
tafelglotzer <nico.rickenbach@gmail.com>
2017-12-25 15:05:23 +0100

    Updated build

--- src/clothes.php
+++ src/clothes.php
@@ -7,7 +7,7 @@ if (! is_dir('resources/clothes')) {
 $figuremap = new SimpleXMLElement(file_get_contents('resources/gamedata/figuremap.xml'));
 
 foreach ($figuremap as $item) {
-    $url = sprintf('https://images.habbo.com/gordon/PRODUCTION-201706211508-77749782/%s.swf', $item->attributes()->id);
+    $url  = sprintf('https://images.habbo.com/gordon/PRODUCTION-201712190406-429122396/%s.swf', $item->attributes()->id);
     $file = sprintf('resources/clothes/%s.swf', $item->attributes()->id);
 
     $files[$file] = $url;
