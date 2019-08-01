42182a9908f279544eadc65d9f9bf33910cb6d60
higoka <nico.rickenbach@gmail.com>
2019-08-01 17:36:42 +0200

    Improved RegEx match + minor changes

--- src/badges.php
+++ src/badges.php
@@ -4,13 +4,13 @@ if (! is_dir('resources/badges')) {
     mkdir('resources/badges', 0777, true);
 }
 
-$texts = file_get_contents('resources/gamedata/external_flash_texts.txt');
+$text = file_get_contents('resources/gamedata/external_flash_texts.txt');
 
-preg_match_all('~(?|(\w+)_badge_name|badge_name_(\w+)|(\w+)_badge_desc|badge_desc_(\w+))~', $texts, $matches);
+preg_match_all('~(?<=badge_(name|desc)_)[\w ]+|[\w ]+(?=_badge_(name|desc))~', $text, $match);
 
-foreach (array_unique($matches[1]) as $badge) {
-    $src = "https://images.habbo.com/c_images/album1584/{$badge}.gif";
-    $dst = "resources/badges/{$badge}.gif";
+foreach ($match[0] as $code) {
+    $src = "https://images.habbo.com/c_images/album1584/{$code}.gif";
+    $dst = "resources/badges/{$code}.gif";
 
     $files[$src] = $dst;
 }
