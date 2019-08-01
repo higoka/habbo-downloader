7f3ff05e4aac78d715b5d6dc53ab9ab626bce799
higoka <nico.rickenbach@gmail.com>
2019-08-01 17:46:12 +0200

    Bugfix

--- src/badges.php
+++ src/badges.php
@@ -9,6 +9,8 @@ $text = file_get_contents('resources/gamedata/external_flash_texts.txt');
 preg_match_all('~(?<=badge_(name|desc)_)[\w ]+|[\w ]+(?=_badge_(name|desc))~', $text, $match);
 
 foreach ($match[0] as $code) {
+    $code = trim($code);
+
     $src = "https://images.habbo.com/c_images/album1584/{$code}.gif";
     $dst = "resources/badges/{$code}.gif";
 
