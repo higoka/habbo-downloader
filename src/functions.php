a4a44b45e29d5b73f21c3466b7b614044e143ca0
higoka <nico.rickenbach@gmail.com>
2019-08-30 21:51:57 +0200

    parse current production automatically

--- src/functions.php
+++ src/functions.php
@@ -18,3 +18,22 @@ function fetch(array $files, bool $override = false)
         copy($src, $dst, $context);
     }
 }
+
+function parseProduction()
+{
+    echo "parsing current production...\n";
+
+    $context = stream_context_create([
+        'http' => [
+            'user_agent' => 'habbo-sucks',
+        ],
+    ]);
+
+    $var = file_get_contents('https://www.habbo.com/gamedata/external_variables/1', false, $context);
+
+    if (! preg_match('~PRODUCTION-[^/]+~i', $var, $match)) {
+        exit("error parsing production\n");
+    }
+
+    return $match[0];
+}
