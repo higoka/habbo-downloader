4d1fe28c117ca35742bbf5ab99412dc1e6694905
higoka <nico.rickenbach@gmail.com>
2019-08-31 12:29:40 +0200

    fix gamedata get downloaded twice

--- src/gamedata.php
+++ src/gamedata.php
@@ -4,12 +4,10 @@ if (! is_dir('resources/gamedata')) {
     mkdir('resources/gamedata', 0777, true);
 }
 
-$files = [
+fetch([
     'https://www.habbo.com/gamedata/furnidata_xml/1'        => 'resources/gamedata/furnidata.xml',
     'https://www.habbo.com/gamedata/external_variables/1'   => 'resources/gamedata/external_variables.txt',
     'https://www.habbo.com/gamedata/external_flash_texts/1' => 'resources/gamedata/external_flash_texts.txt',
     "https://images.habbo.com/gordon/{$prod}/figuremap.xml" => 'resources/gamedata/figuremap.xml',
     "https://images.habbo.com/gordon/{$prod}/effectmap.xml" => 'resources/gamedata/effectmap.xml',
-];
-
-fetch($files, true);
+], true);
