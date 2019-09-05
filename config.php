e5170bb5294bd19eb6dd90c09169a49006e79d61
higoka <nico.rickenbach@gmail.com>
2019-09-06 01:03:14 +0200

    move configuration to separate file

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    /**
+     * domain you want to download from
+     */
+    'domain' => 'com',
+
+    /**
+     * enable or disable debugging mode
+     */
+    'debug' => false,
+];
--- src/gamedata.php
+++ src/gamedata.php
@@ -5,12 +5,12 @@ if (! is_dir('resources/gamedata')) {
 }
 
 fetch([
-    "https://www.habbo.{$domain}/gamedata/external_variables/0"                     => 'resources/gamedata/external_variables.txt',
-    "https://www.habbo.{$domain}/gamedata/external_flash_texts/0"                   => 'resources/gamedata/external_flash_texts.txt',
-    "https://www.habbo.{$domain}/gamedata/override/external_override_variables/0"   => 'resources/gamedata/external_override_variables.txt',
-    "https://www.habbo.{$domain}/gamedata/override/external_flash_override_texts/0" => 'resources/gamedata/external_flash_override_texts.txt',
-    "https://www.habbo.{$domain}/gamedata/furnidata_xml/0"                          => 'resources/gamedata/furnidata.xml',
-    "https://www.habbo.{$domain}/gamedata/productdata/0"                            => 'resources/gamedata/productdata.txt',
-    "https://images.habbo.com/gordon/{$prod}/figuremap.xml"                         => 'resources/gamedata/figuremap.xml',
-    "https://images.habbo.com/gordon/{$prod}/effectmap.xml"                         => 'resources/gamedata/effectmap.xml',
+    "https://www.habbo.{$config['domain']}/gamedata/external_variables/0"                     => 'resources/gamedata/external_variables.txt',
+    "https://www.habbo.{$config['domain']}/gamedata/external_flash_texts/0"                   => 'resources/gamedata/external_flash_texts.txt',
+    "https://www.habbo.{$config['domain']}/gamedata/override/external_override_variables/0"   => 'resources/gamedata/external_override_variables.txt',
+    "https://www.habbo.{$config['domain']}/gamedata/override/external_flash_override_texts/0" => 'resources/gamedata/external_flash_override_texts.txt',
+    "https://www.habbo.{$config['domain']}/gamedata/furnidata_xml/0"                          => 'resources/gamedata/furnidata.xml',
+    "https://www.habbo.{$config['domain']}/gamedata/productdata/0"                            => 'resources/gamedata/productdata.txt',
+    "https://images.habbo.com/gordon/{$prod}/figuremap.xml"                                   => 'resources/gamedata/figuremap.xml',
+    "https://images.habbo.com/gordon/{$prod}/effectmap.xml"                                   => 'resources/gamedata/effectmap.xml',
 ], true);
