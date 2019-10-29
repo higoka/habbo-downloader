b39fa1ab4ed611fb4009225d6da58389e37bc484
higoka <nico.rickenbach@gmail.com>
2019-10-29 17:17:19 +0100

    decrease limit

--- src/mp3.php
+++ src/mp3.php
@@ -4,7 +4,7 @@ if (! is_dir('resources/mp3')) {
     mkdir('resources/mp3', 0777, true);
 }
 
-for ($i = 0; $i <= 800; $i++) {
+for ($i = 0; $i <= 750; $i++) {
     $src = "https://images.habbo.com/dcr/hof_furni/mp3/sound_machine_sample_{$i}.mp3";
     $dst = "resources/mp3/sound_machine_sample_{$i}.mp3";
 
