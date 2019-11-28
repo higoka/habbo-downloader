79f18ac8fe57fb9ac35645bf60d2beab2e2716a6
higoka <nico.rickenbach@gmail.com>
2019-11-28 17:30:19 +0100

    increase limit

--- src/icons.php
+++ src/icons.php
@@ -4,7 +4,7 @@ if (! is_dir('resources/icons')) {
     mkdir('resources/icons', 0777, true);
 }
 
-for ($i = 1; $i <= 300; $i++) {
+for ($i = 1; $i <= 350; $i++) {
     $src = "https://images.habbo.com/c_images/catalogue/icon_{$i}.png";
     $dst = "resources/icons/icon_{$i}.png";
 
