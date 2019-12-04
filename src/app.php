0b5961ff3e02d74d7f4aa5ef1fea1f560b74bb2e
Salomão Neto <salomaosnff@yahoo.com>
2019-12-04 03:29:47 -0300

    Download Progress, Terminal colors, Help

--- src/app.php
+++ src/app.php
@@ -1,18 +1,19 @@
 <?php
 
-$config = require_once 'config/app.php';
+$config = require_once "config/app.php";
 
-if ($config['debug']) {
+if ($config["debug"]) {
     error_reporting(E_ALL);
-    ini_set('display_errors', true);
-    ini_set('display_startup_errors', true);
+    ini_set("display_errors", true);
+    ini_set("display_startup_errors", true);
 } else {
     error_reporting(0);
-    ini_set('display_errors', false);
-    ini_set('display_startup_errors', false);
+    ini_set("display_errors", false);
+    ini_set("display_startup_errors", false);
 }
 
-require_once 'src/functions.php';
+require_once "src/functions.php";
+require_once "src/Terminal.php";
 
 echo "
    __        __   __
@@ -27,40 +28,91 @@ initializing...\n";
 
 $prod = parseProduction();
 
-require_once 'src/gamedata.php';
+require_once "src/gamedata.php";
 
-echo "\n\nenter a command: ";
+$commands = array(
+    "badges" => array(
+        "help" => "Download all badges",
+        "script" => "src/badges.php"
+    ),
+    "furnitures" => array(
+        "help" => "Download all furnitures",
+        "script" => "src/furnitures.php"
+    ),
+    "clothes" => array(
+        "help" => "Download all clothes",
+        "script" => "src/clothes.php"
+    ),
+    "effects" => array(
+        "help" => "Download all effects",
+        "script" => "src/effects.php"
+    ),
+    "icons" => array(
+        "help" => "Download all icons",
+        "script" => "src/icons.php"
+    ),
+    "mp3" => array(
+        "help" => "Download all sounds",
+        "script" => "src/mp3.php"
+    ),
+    "hotelview" => array(
+        "help" => "Download all hotelview images",
+        "script" => "src/hotelview.php"
+    ),
+    "ficon" => array(
+        "help" => "Download all furniture icons",
+        "script" => "src/ficon.php"
+    ),
+    "gamedata" => array(
+        "help" => "Download all gamedata files",
+        "script" => "src/gamedata.php"
+    ),
+    "help" => array(
+        "help" => "Show this help"
+    ),
+    "exit" => array(
+        "help" => "Quit application"
+    ),
+);
 
-switch (trim(fgets(STDIN))) {
-    case 'badges':
-        require_once 'src/badges.php';
-        break;
-    case 'furnitures':
-        require_once 'src/furnitures.php';
-        break;
-    case 'clothes':
-        require_once 'src/clothes.php';
-        break;
-    case 'effects':
-        require_once 'src/effects.php';
-        break;
-    case 'icons':
-        require_once 'src/icons.php';
-        break;
-    case 'mp3':
-        require_once 'src/mp3.php';
-        break;
-    case 'hotelview':
-        require_once 'src/hotelview.php';
-        break;
-    case 'ficon':
-        require_once 'src/ficon.php';
-        break;
-    case 'gamedata':
-        require_once 'src/gamedata.php';
-        break;
-    default:
-        exit("\ninvalid command\n");
+function prompt ($message) {
+    echo $message;
+
+    while (!$input = trim(fgets(STDIN))) {
+        echo $message;
+    }
+    return $input;
 }
 
-exit("\n\nDone!\n");
+echo "\x1b[33mNeed Help? Enter \"help\".\x1b[0m\n\n";
+
+while (true) {
+    $input = prompt("Enter a command: ");
+
+    if (isset($commands[$input]) && isset($commands[$input]["script"])) {
+        $command = $commands[$input];
+        require_once $command["script"];
+    }
+
+    if ($input == "exit") {
+        exit("\x1b[33mBye :)\x1b[0m\n");
+    }
+
+    if ($input == "help") {
+        $max_width = max(array_map('strlen', array_keys($commands))) + 4;
+        $line = str_repeat("=", Terminal::getWidth());
+        echo "\n\x1b[1mHabbo Downloader Help:\n\x1b[0m\n";
+        echo $line;
+        echo "\n\x1b[1m".str_pad("Command", $max_width, " ")."Description\x1b[0m\n";
+        echo $line;
+        foreach ($commands as $cmd => $attrs) {
+            $cmd = str_pad($cmd, $max_width, ' ');
+            $help = $attrs["help"];
+            echo "$cmd$help\n";
+        }
+        echo "\n";
+        continue;
+    }
+
+    echo "\x1b[31mInvalid command!\x1b[0m\n";
+}
--- src/functions.php
+++ src/functions.php
@@ -1,15 +1,55 @@
 <?php
 
+require_once __DIR__.'/Terminal.php';
+
+function create_progress (string $format) {
+    $current_char = '=';
+    $bar_char = '.';
+    $bar_token = '{bar}';
+    $width = Terminal::getWidth();
+
+    return function (int $current, int $total, array $tokens = array()) use ($format, $current_char, $bar_token, $bar_char, $width) {
+        $tokens['current'] = $current;
+        $tokens['total'] = $total;
+        $tokens['percent'] = number_format(($current / $total) * 100, 1, '.', '')."%";
+        
+        $progress_bar = preg_replace_callback("|{(\w+)}|", function(array $matches) use ($tokens) {
+            $token = $matches[1];
+            return isset($tokens[$token]) ? $tokens[$token] : $matches[0];
+        }, $format);
+
+        $progress_width = $width - strlen($progress_bar) - strlen($bar_token);
+        $filled_width = ceil($progress_width * ($current / $total));
+        $filled = array_fill(0, $filled_width, $current_char);
+        $bar = array_fill(0, $progress_width, $bar_char);
+        array_splice($bar, 0, $filled_width, join('', $filled));
+        $bar = join('', $bar);
+        return str_replace($bar_token, $bar, $progress_bar);
+    };
+}
+
 function fetch(array $files, bool $override = false): void
 {
+    $clear_line = "\x1b[F\x1b[0G\x1b[2K";
+    $prog = create_progress("{before}Downloading {file} {percent} [{bar}] {current}/{total}\n");
+
     $context = stream_context_create([
         'http' => [
             'header' => 'user-agent:habbo-sucks',
         ],
     ]);
 
+    $i = 0;
+    $total = count($files);
+
     foreach ($files as $src => $dst) {
-        echo "\n> downloading: $src";
+        $i++;
+        echo call_user_func($prog, $i, $total, array(
+            'file' => substr(str_pad(basename($src), 30, ' '), 0, 30),
+            'before' => $clear_line
+        ));
+
+        // echo "\n> downloading $i of $total: $src";
 
         if (is_file($dst) && $override === false) {
             continue;
