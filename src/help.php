47144f2b23e784862bda7c10e65ac17346bc1813
higoka <nico.rickenbach@gmail.com>
2020-02-23 13:28:36 +0100

    update command list

--- src/help.php
+++ src/help.php
@@ -7,6 +7,7 @@ echo "\n> clothes           Download the latest Habbo Clothes";
 echo "\n> effects           Download the latest Habbo Effects";
 echo "\n> icons             Download the latest Habbo Catalogue Icons";
 echo "\n> mp3               Download the latest Habbo sound_machine_sample";
+echo "\n> pets              Download the latest Habbo Pets";
 echo "\n> hotelview         Download the latest Habbo Hotelview / Reception";
 echo "\n> gamedata          Download the latest Habbo Gamedata";
 echo "\n> habboswf          Download the latest Habbo.swf";
