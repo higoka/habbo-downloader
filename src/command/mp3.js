f4b57191433dc7d8e024c09cc609123edc838837
higoka <nico.rickenbach@gmail.com>
2021-01-30 14:38:01 +0100

    refactor mp3 command

--- src/command/mp3.js
+++ src/command/mp3.js
@@ -1,20 +1,10 @@
-const { fetchOne } = require('../functions')
+const { fetchUntil } = require('../functions')
 
 async function handle () {
-  let failed = 0
-  let i = 1
-
-  while (failed < 3) {
-    try {
-      const msg = await fetchOne(`https://images.habbo.com/dcr/hof_furni/mp3/sound_machine_sample_${i}.mp3`, `resource/dcr/hof_furni/mp3/sound_machine_sample_${i}.mp3`)
-      failed = 0
-      console.log(msg)
-    } catch (err) {
-      console.log(err.message)
-      failed++
-    }
-    i++
-  }
+  await fetchUntil({
+    src: 'https://images.habbo.com/dcr/hof_furni/mp3/sound_machine_sample_%i%.mp3',
+    dst: 'resource/dcr/hof_furni/mp3/sound_machine_sample_%i%.mp3',
+  })
 }
 
 module.exports = handle
